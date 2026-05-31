9556fc3e650b092f23d1c21acd7833e68665acc2
aashumalik784 <aashumalik784@gmail.com>
2026-05-31 21:08:49 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,2 @@
-base: '/',+O
-+import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-}+O
-
-+// @ts-ignore just absorb
+// @ts-ignore just absorb
 export { default } from "./vite.config.client.js"
