5cca898f73fbcc5510906b8d44a40711dc03f72f
AashuMalik <aashu@example.com>
2026-05-31 18:32:42 +0530

    fix white screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,11 @@
-// @ts-ignore just absorb
+base: '/',+O
++import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+}+O
+
++// @ts-ignore just absorb
 export { default } from "./vite.config.client.js"
